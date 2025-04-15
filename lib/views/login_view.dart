97d28b8eb44a049116f90204f1549a4a38d2ca01
glib <glegalchenko@gmail.com>
2025-04-14 20:44:28 -0600

    step-7

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
+// import 'dart:developer' as devtools show log;
 import '../constants/routes.dart';
 import '../utilities/show_error_dialog.dart';
 
