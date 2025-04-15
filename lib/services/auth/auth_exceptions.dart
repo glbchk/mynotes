9f3928d0b75a7a07cd6e3de340732cec128caee4
glib <glegalchenko@gmail.com>
2025-04-14 22:29:11 -0600

    step-8

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,18 @@
+// login exceptions
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordException implements Exception {}
+
+// register exceptions
+
+class WeakPasswordException implements Exception {}
+
+class EmailAlreadyInUseException implements Exception {}
+
+class InvalidEmailException implements Exception {}
+
+// generic exceptions
+
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
