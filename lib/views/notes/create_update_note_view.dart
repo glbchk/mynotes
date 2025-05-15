282ac3aabcf9bf71027be26972805b612f736908
glib <glegalchenko@gmail.com>
2025-05-14 21:41:39 -0600

    step-19

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -53,7 +53,7 @@ class _CreateUpdateNoteVIewState extends State<CreateUpdateNoteVIew> {
       return exisitingNote;
     }
     final currentUser = AuthService.firebase().currentUser!;
-    final email = currentUser.email!;
+    final email = currentUser.email;
     final owner = await _notesService.getUser(email: email);
 
     final newNote = await _notesService.createNote(owner: owner);
